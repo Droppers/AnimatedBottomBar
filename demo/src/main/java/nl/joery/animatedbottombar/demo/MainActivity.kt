09449c1babd5c01d2d13c248ba333bd2e7825673
Joery <joery@droppe.rs>
2020-02-29 14:27:33 +0100

    Renamed a lot of package names

---
+++ demo/src/main/java/nl/joery/animatedbottombar/demo/MainActivity.kt
@@ -0,0 +1,34 @@
+package nl.joery.animatedbottombar.demo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+
+import kotlinx.android.synthetic.main.activity_main.*
+import nl.joery.animatedbottombar.R
+import nl.joery.library.animatedbottombar.AnimatedBottomBar
+import nl.joery.library.animatedbottombar.BottomBarStyle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        select.setOnClickListener {
+            bottomBar.addTab(
+                AnimatedBottomBar.Tab(
+                    ContextCompat.getDrawable(applicationContext,
+                        R.drawable.alarm
+                    ),
+                    "Added Tab"
+                )
+            )
+        }
+
+        deselect.setOnClickListener {
+            val tab = bottomBar.tabs.last()
+            bottomBar.removeTab(tab)
+        }
+    }
+}
