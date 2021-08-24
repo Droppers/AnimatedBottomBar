9ef845ffb601e223ca36f8c819beafcfee370d7e
O. Khmaruk <pelmenstar1@gmail.com>
2021-08-24 23:30:40 +0300

    Improve animation performance of TabIndicator
    - Setting value of field (sDurationScale) than calling setter (setDurationScale).
    - Animator can be cached
    - Add android.util.Property to set currentLeft. It should be faster than way with update listener, because there is no boxing

--- library/src/main/java/nl/joery/animatedbottombar/TabIndicator.kt
+++ library/src/main/java/nl/joery/animatedbottombar/TabIndicator.kt
@@ -1,29 +1,37 @@
 package nl.joery.animatedbottombar
 
-import android.animation.ValueAnimator
+import android.animation.ObjectAnimator
 import android.graphics.Canvas
 import android.graphics.Paint
 import android.graphics.Path
 import android.graphics.RectF
+import android.os.Build
+import android.util.FloatProperty
+import android.util.Property
 import androidx.recyclerview.widget.RecyclerView
 import nl.joery.animatedbottombar.utils.fixDurationScale
-import kotlin.math.abs
-
 
 internal class TabIndicator(
     private val bottomBar: AnimatedBottomBar,
     private val parent: RecyclerView,
     private val adapter: TabAdapter
-) :
-    RecyclerView.ItemDecoration() {
-    private lateinit var paint: Paint
-    private var corners: FloatArray? = null
-    private var animator: ValueAnimator? = null
+) : RecyclerView.ItemDecoration() {
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG).apply {
+        style = Paint.Style.FILL
+    }
+
+    private val corners = FloatArray(8)
+    private val animator = ObjectAnimator().apply {
+        target = this@TabIndicator
+        setProperty(CURRENT_LEFT_PROPERTY)
+        fixDurationScale()
+    }
 
     private var lastSelectedIndex: Int = RecyclerView.NO_POSITION
     private var currentLeft: Float = 0f
 
-    private val indicatorRect: RectF = RectF(0f, 0f, 0f, 0f)
+    private val indicatorRect = RectF()
+    private val roundRectPath = Path()
 
     private val shouldRender: Boolean
         get() = bottomBar.indicatorStyle.indicatorAppearance != AnimatedBottomBar.IndicatorAppearance.INVISIBLE
@@ -33,74 +41,86 @@ internal class TabIndicator(
     }
 
     override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
-        super.onDrawOver(c, parent, state)
-
         if (adapter.selectedIndex == RecyclerView.NO_POSITION || !shouldRender) {
             return
         }
 
-        val isAnimating = animator?.isRunning == true
-        val animatedFraction = animator?.animatedFraction ?: 1f
+        val isAnimating = animator.isRunning
+        val animatedFraction = animator.animatedFraction
         val lastView = parent.getChildAt(lastSelectedIndex)
         val newView = parent.getChildAt(adapter.selectedIndex) ?: return
-        var currentWidth = newView.width.toFloat()
 
-        if (bottomBar.indicatorAnimation == AnimatedBottomBar.IndicatorAnimation.SLIDE) {
-            if (isAnimating && lastView != null) {
-                currentWidth = lastView.width + (newView.width - lastView.width) * animatedFraction
-                currentLeft = animator?.animatedValue as Float
-            } else {
-                currentLeft = newView.left.toFloat()
-            }
+        val newViewWidth = newView.width.toFloat()
+        val newViewLeft = newView.left.toFloat()
 
-            drawIndicator(c, currentLeft, currentWidth)
-        } else if (bottomBar.indicatorAnimation == AnimatedBottomBar.IndicatorAnimation.FADE) {
-            if (isAnimating && lastView != null) {
-                val alpha = 255
-                val lastAlpha = alpha - alpha * animatedFraction
-                val newAlpha = alpha * animatedFraction
-                drawIndicator(
-                    c,
-                    lastView.left.toFloat(),
-                    lastView.width.toFloat(),
-                    lastAlpha.toInt()
-                )
-                drawIndicator(c, newView.left.toFloat(), newView.width.toFloat(), newAlpha.toInt())
-            } else {
-                drawIndicator(c, newView.left.toFloat(), newView.width.toFloat())
+        var currentWidth = newViewWidth
+
+        when(bottomBar.indicatorAnimation) {
+            AnimatedBottomBar.IndicatorAnimation.SLIDE -> {
+                if (isAnimating && lastView != null) {
+                    val lastViewWidth = lastView.width.toFloat()
+                    currentWidth =
+                        lastViewWidth + (newViewWidth - lastViewWidth) * animatedFraction
+                } else {
+                    currentLeft = newViewLeft
+                }
+
+                drawIndicator(c, currentLeft, currentWidth)
             }
-        } else {
-            drawIndicator(c, newView.left.toFloat(), newView.width.toFloat())
+            AnimatedBottomBar.IndicatorAnimation.FADE -> {
+                if (isAnimating && lastView != null) {
+                    val newAlpha = 255f * animatedFraction
+                    val lastAlpha = 255f - newAlpha
+
+                    drawIndicator(
+                        c,
+                        lastView.left.toFloat(),
+                        lastView.width.toFloat(),
+                        lastAlpha.toInt()
+                    )
+                    drawIndicator(
+                        c,
+                        newViewLeft,
+                        newViewWidth,
+                        newAlpha.toInt()
+                    )
+                } else {
+                    drawIndicator(c, newViewLeft, newViewWidth)
+                }
+            }
+            else -> {
+                drawIndicator(c, newViewLeft, newViewWidth)
+            }
         }
     }
 
     private fun drawIndicator(c: Canvas, viewLeft: Float, viewWidth: Float, alpha: Int = 255) {
+        val indicatorMargin = bottomBar.indicatorStyle.indicatorMargin
         indicatorRect.set(
-            viewLeft + bottomBar.indicatorStyle.indicatorMargin,
+            viewLeft + indicatorMargin,
             getTop(),
-            viewLeft + viewWidth - bottomBar.indicatorStyle.indicatorMargin,
+            viewLeft + viewWidth - indicatorMargin,
             getBottom()
         )
 
-        paint.alpha = when {
-            alpha < 0 -> abs(alpha)
-            alpha > 255 -> 255 - (alpha - 255)
-            else -> alpha
-        }
+        paint.alpha = alpha
 
-        if (bottomBar.indicatorStyle.indicatorAppearance == AnimatedBottomBar.IndicatorAppearance.SQUARE) {
-            c.drawRect(
-                indicatorRect, paint
-            )
-
-        } else if (bottomBar.indicatorStyle.indicatorAppearance == AnimatedBottomBar.IndicatorAppearance.ROUND) {
-            val path = Path()
-            path.addRoundRect(
-                indicatorRect,
-                corners!!,
-                Path.Direction.CW
-            )
-            c.drawPath(path, paint)
+        when(bottomBar.indicatorStyle.indicatorAppearance) {
+            AnimatedBottomBar.IndicatorAppearance.SQUARE -> {
+                c.drawRect(indicatorRect, paint)
+            }
+            AnimatedBottomBar.IndicatorAppearance.ROUND -> {
+                val path = roundRectPath
+                path.rewind()
+                path.addRoundRect(
+                    indicatorRect,
+                    corners,
+                    Path.Direction.CW
+                )
+                c.drawPath(path, paint)
+            }
+            else -> {
+            }
         }
     }
 
@@ -109,27 +129,41 @@ internal class TabIndicator(
             AnimatedBottomBar.IndicatorLocation.TOP ->
                 0f
             AnimatedBottomBar.IndicatorLocation.BOTTOM ->
-                parent.height - bottomBar.indicatorStyle.indicatorHeight.toFloat()
+                (parent.height - bottomBar.indicatorStyle.indicatorHeight).toFloat()
         }
     }
 
-    private fun getCorners(): FloatArray? {
-        val radius = bottomBar.indicatorStyle.indicatorHeight.toFloat()
-        return when (bottomBar.indicatorStyle.indicatorLocation) {
-            AnimatedBottomBar.IndicatorLocation.TOP ->
-                floatArrayOf(
-                    0f, 0f,
-                    0f, 0f,
-                    radius, radius,
-                    radius, radius
-                )
-            AnimatedBottomBar.IndicatorLocation.BOTTOM ->
-                floatArrayOf(
-                    radius, radius,
-                    radius, radius,
-                    0f, 0f,
-                    0f, 0f
-                )
+    private fun updateCorners() {
+        val indicatorStyle = bottomBar.indicatorStyle
+        val radius = indicatorStyle.indicatorHeight.toFloat()
+
+        when (indicatorStyle.indicatorLocation) {
+            AnimatedBottomBar.IndicatorLocation.TOP -> {
+                corners.run {
+                    this[0] = 0f
+                    this[1] = 0f
+                    this[2] = 0f
+                    this[3] = 0f
+
+                    this[4] = radius
+                    this[5] = radius
+                    this[6] = radius
+                    this[7] = radius
+                }
+            }
+            AnimatedBottomBar.IndicatorLocation.BOTTOM -> {
+                corners.run {
+                    this[0] = radius
+                    this[1] = radius
+                    this[2] = radius
+                    this[3] = radius
+
+                    this[4] = 0f
+                    this[5] = 0f
+                    this[6] = 0f
+                    this[7] = 0f
+                }
+            }
         }
     }
 
@@ -143,8 +177,8 @@ internal class TabIndicator(
     }
 
     fun setSelectedIndex(lastIndex: Int, newIndex: Int, animate: Boolean) {
-        if (animator?.isRunning == true) {
-            animator?.cancel()
+        if (animator.isRunning) {
+            animator.cancel()
         }
 
         if (!shouldRender) {
@@ -153,33 +187,47 @@ internal class TabIndicator(
 
         val newView = parent.getChildAt(newIndex)
         if (!animate || lastIndex == -1 || newView == null) {
-            parent.postInvalidate()
+            parent.invalidate()
             return
         }
 
         lastSelectedIndex = lastIndex
 
-        animator = ValueAnimator.ofFloat(currentLeft, newView.left.toFloat()).apply {
+        animator.run {
+            setFloatValues(currentLeft, newView.left.toFloat())
             duration = bottomBar.tabStyle.animationDuration.toLong()
             interpolator = bottomBar.tabStyle.animationInterpolator
-            fixDurationScale()
-            addUpdateListener {
-                parent.postInvalidate()
-            }
+
             start()
         }
     }
 
     fun applyStyle() {
-        paint = Paint().apply {
-            color = bottomBar.indicatorStyle.indicatorColor
-            style = Paint.Style.FILL
-            isAntiAlias = true
-        }
-        corners = getCorners()
+        paint.color = bottomBar.indicatorStyle.indicatorColor
+        updateCorners()
 
         if (shouldRender) {
-            parent.postInvalidate()
+            parent.invalidate()
+        }
+    }
+
+    companion object {
+        private val CURRENT_LEFT_PROPERTY = if(Build.VERSION.SDK_INT >= 24) {
+            object: FloatProperty<TabIndicator>("currentLeft") {
+                override fun get(o: TabIndicator): Float = o.currentLeft
+                override fun setValue(o: TabIndicator, value: Float) {
+                    o.currentLeft = value
+                    o.parent.invalidate()
+                }
+            }
+        } else {
+            object: Property<TabIndicator, Float>(Float::class.java, "currentLeft") {
+                override fun get(o: TabIndicator): Float = o.currentLeft
+                override fun set(o: TabIndicator, value: Float) {
+                    o.currentLeft = value
+                    o.parent.invalidate()
+                }
+            }
         }
     }
 }
